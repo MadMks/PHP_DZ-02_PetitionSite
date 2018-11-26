03a7fbfeb04ab30992c0d15efd8d16ae6b7367c0
Makc <mks59k@gmail.com>
2018-11-26 23:32:45 +0200

    Add project files.

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
-    require_once('db.conf.php');
+	require_once('db.conf.php');
 
-    
+	$sql = 'SELECT petitions.*, users.email AS author_email,
+			petitions
+			FROM petitions
+			LEFT JOIN users 
+			ON (petitions.user_id = users.id)
+			';
+
+	$sth = $dbh->prepare($sql);
+	$sth->execute();
+	$petitions = $sth->fetchAll(PDO::FETCH_OBJ);
+
+	print_r($petitions);	
+
+	foreach ($petitions as $petition) {
+		echo($petition);
+	}
+?>
+<div>
+
+</div>
