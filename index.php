8f60d97950753c67a30a742f3da61560595844a0
Makc <mks59k@gmail.com>
2018-11-26 23:55:48 +0200

    test foreach 1

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 	require_once('db.conf.php');
 
-	$sql = 'SELECT petitions.*, users.email AS author_email,
-			petitions
+	$sql = 'SELECT petitions.*, users.email AS author_email
 			FROM petitions
 			LEFT JOIN users 
 			ON (petitions.user_id = users.id)
@@ -15,9 +14,12 @@
 	print_r($petitions);	
 
 	foreach ($petitions as $petition) {
-		echo($petition);
+		echo("<br>");
+		echo($petition->title);
 	}
 ?>
 <div>
-
+	<?php foreach($petitions as $petition) {?>
+		<div><?php $petition->title ?></div>
+	<?php } ?>
 </div>
