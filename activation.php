eefb26bcbf21ac54e2aec5efc33dcdea7549b8b6
Makc <mks59k@gmail.com>
2018-11-29 15:23:43 +0200

    Вывод на главной только активированых петиций.

--- activation.php
+++ activation.php
@@ -1,4 +1,29 @@
 <?php
     if (isset($_GET['id']) && isset($_GET['token'])) {
         
+        require_once('db.conf.php');
+
+        $petitionId = $_GET['id'];
+        $token = $_GET['token'];
+
+        $sth = $dbh->prepare(
+            "SELECT * FROM state_of_petitions
+            WHERE petition_id = :petitionId"
+        );
+        $sth->bindValue(':petitionId', $petitionId);
+        $sth->execute();
+        $petitionState = $sth->fetch(PDO::FETCH_ASSOC);
+
+        if (!empty($petitionState)) {
+            if ($petitionState['activationKey'] == $token) {
+                // Активация петиции.
+                $sth = $dbh->prepare(
+                    'UPDATE state_of_petitions
+                    SET active = 1
+                    WHERE petition_id = :petitionId'
+                );
+                $sth->bindValue(':petitionId', $petitionId);
+                $sth->execute();
+            }
+        }
     }
--- petitions.php
+++ petitions.php
@@ -4,7 +4,10 @@
 	$sql = 'SELECT petitions.*, users.email AS author_email
 			FROM petitions
 			LEFT JOIN users 
-			ON (petitions.user_id = users.id)
+			  ON (petitions.user_id = users.id)
+            LEFT JOIN state_of_petitions AS statePetition
+              ON (petitions.id = statePetition.petition_id)
+            WHERE statePetition.active = 1
 			';
 
 	$sth = $dbh->prepare($sql);
